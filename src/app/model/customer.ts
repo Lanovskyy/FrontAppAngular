16619fdbe736b90bfce925f358d0e88fc4d94c4b
CaioGabrielZzz <caiopimenta130@gmail.com>
2023-05-09 22:21:48 -0300

    First commit

---
+++ src/app/model/customer.ts
@@ -0,0 +1,12 @@
+export interface Customer {
+    idCustomer?: any;
+    firstNameCustomer: string;
+    lastNameCustomer: string;
+    cpfCustomer: any;
+    birthDateCustomer: any;
+    dateCreatedCustomer: any;
+    monthlyIncomeCustomer: string;
+    statusCustomer: boolean;
+    emailCustomer: string;
+    passwordCustomer: string;
+}
